f73c9a9a91c8ec6b06056aadd2ee6ee4fc6e9528
Ita Brasil <catataubra@icloud.com>
2024-07-24 17:30:08 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print ("olá, mundo")
+
+# Solicita ao usuário para inserir dois números 
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+# Calcula a soma dos dois números
+soma = numero1 + numero2
+
+# Imprima a soma
+print ("A soma é", soma) 
